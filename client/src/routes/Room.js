4a47543f466fcf78f902514f782681d00526363f
Chaim <calvinfriedman@Calvins-MacBook-Pro-2.local>
2020-04-26 11:55:53 -0400

    small changes

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import Peer from "simple-peer";
 import styled from "styled-components";
+import streamSaver from "streamsaver";
 
 const Container = styled.div`
     padding: 20px;
@@ -14,36 +15,30 @@ const Container = styled.div`
 
 const Room = (props) => {
     const [connectionEstablished, setConnection] = useState(false);
+    const [file, setFile] = useState();
+    const [gotFile, setGotFile] = useState(false);
+
+    const chunksRef = useRef([]);
     const socketRef = useRef();
     const peersRef = useRef([]);
+    const peerRef = useRef();
+    const fileNameRef = useRef("");
+
     const roomID = props.match.params.roomID;
 
     useEffect(() => {
         socketRef.current = io.connect("/");
         socketRef.current.emit("join room", roomID);
         socketRef.current.on("all users", users => {
-            users.forEach(userID => {
-                const peer = createPeer(userID, socketRef.current.id);
-                peersRef.current.push({
-                    peerID: userID,
-                    peer,
-                });
-            });
+            peerRef.current = createPeer(users[0], socketRef.current.id);
         });
 
         socketRef.current.on("user joined", payload => {
-            setTimeout(() => {
-                const peer = addPeer(payload.signal, payload.callerID);
-                peersRef.current.push({
-                    peerID: payload.callerID,
-                    peer,
-                });
-            }, 10);
+            peerRef.current = addPeer(payload.signal, payload.callerID);
         });
 
         socketRef.current.on("receiving returned signal", payload => {
-            const item = peersRef.current.find(p => p.peerID === payload.id);
-            item.peer.signal(payload.signal);
+            peerRef.current.signal(payload.signal);
             setConnection(true);
         });
 
@@ -51,9 +46,6 @@ const Room = (props) => {
             alert("room is full");
         })
 
-        socketRef.current.on("user left", peerID => {
-           
-        })
     }, []);
 
     function createPeer(userToSignal, callerID) {
@@ -65,6 +57,8 @@ const Room = (props) => {
         peer.on("signal", signal => {
             socketRef.current.emit("sending signal", { userToSignal, callerID, signal });
         });
+
+        peer.on("data", handleReceivingData);
 
         return peer;
     }
@@ -79,15 +73,36 @@ const Room = (props) => {
             socketRef.current.emit("returning signal", { signal, callerID });
         });
 
+        peer.on("data", handleReceivingData);
+
         peer.signal(incomingSignal);
         setConnection(true);
         return peer;
     }
 
+    function handleReceivingData(data) {
+        
+    }
+
+    function download() {
+       
+    }
+
+    function selectFile(e) {
+
+    }
+
+    function sendFile() {
+       
+    }
+
     let body;
     if (connectionEstablished) {
         body = (
-            <input type="file" />
+            <div>
+                <input onChange={selectFile} type="file" />
+                <button onClick={sendFile}>Send file</button>
+            </div>
         );
     } else {
         body = (
@@ -95,9 +110,21 @@ const Room = (props) => {
         );
     }
 
+
+    let downloadPrompt;
+    if (gotFile) {
+        downloadPrompt = (
+            <div>
+                <span>You have received a file. Would you like to download the file?</span>
+                <button onClick={download}>Yes</button>
+            </div>
+        );
+    }
+
     return (
         <Container>
             {body}
+            {downloadPrompt}
         </Container>
     );
 };
